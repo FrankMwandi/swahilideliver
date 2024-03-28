329d99514d0f3be10bd1bcaf9e0a6a3ef8fbca7f
mwandi <frankmwandi93@gmail.com>
2024-03-29 01:37:08 +0300

    lightmode theme

---
+++ lib/Themes/light_mode.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+ThemeData lightmode = ThemeData(
+  colorScheme: ColorScheme.light(
+    background: Colors.grey[300] ?? Color(0xFFE0E0E0),
+    primary: Colors.grey[500] ?? Color(0xFF9E9E9E),
+    secondary: Colors.grey[100] ?? Color(0xFFFAFAFA),
+    tertiary: Colors.white,
+    inversePrimary: Colors.grey[700] ?? Color(0xFF616161),
+  ),
+);
+
