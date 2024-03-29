7aab70c375c03ebdbf069ae9f1287eae78fefb03
mwandi <frankmwandi93@gmail.com>
2024-03-29 20:39:01 +0300

    using custom button and text in login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,14 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import "package:flutter/material.dart";
+import "package:swahilideliver/components/my_button.dart";
+import "package:swahilideliver/components/my_text_field.dart";
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+   LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +21,48 @@ class LoginPage extends StatelessWidget {
             // logo
             Icon(
               Icons.lock_clock_rounded,
-              size: 72,
+              size: 100,
               color: Theme.of(context).colorScheme.inversePrimary
               ),
+              const SizedBox(height: 25),
+            //slogan
+            Text('Swahili Delivery App',
+            style: TextStyle(
+              fontSize: 16,
+              color: Theme.of(context).colorScheme.inversePrimary
+             ),
+            ),
+            //Email field
+            MyTextField(
+              controller: emailController,
+              hintText:'Email',
+              obscureText: false,
+            ),
+            //Password field
+            const SizedBox(height:25),
+            MyTextField(
+              controller: passwordController,
+              hintText:'Password',
+              obscureText: true,
+            ),
+            const SizedBox(height: 25),
+            //Signing button
+            MyButton(onTap: (){}, text: 'Login'),
+            //Recover passwor or Signup
+            const SizedBox(height: 25),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Not a Member?",
+                style:TextStyle(color: Theme.of(context).colorScheme.inversePrimary),
+                
+                ),
+                const SizedBox(width: 4.0),
+                Text('Register new Account',
+                style:TextStyle(color: Theme.of(context).colorScheme.inversePrimary),
+                )
+              ],
+            )
         
           ],
           ),
